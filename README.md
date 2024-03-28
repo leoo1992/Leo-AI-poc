a0e3abbc5e1d61770f02728de95380f91d85a7f3
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-03-28 10:20:00 -0300

    bug fixes

--- README.md
+++ README.md
@@ -2,16 +2,17 @@
 
 Este projeto é um clone do Gemini do Google com reconhecimento por voz e seleção de temas. Foi desenvolvido utilizando React, TypeScript, Vite, Tailwind CSS.
 
+[![Assista ao vídeo](https://cdn.loom.com/sessions/thumbnails/2885c50d19574b9896198a537294792c-with-play.gif)](https://www.loom.com/share/2885c50d19574b9896198a537294792c)
+
+## Deploy
+
+A aplicação está disponível no Vercel: [Léo-AI POC Beta](https://chat-gpt-poc-beta.vercel.app/)
 
 ## Funcionalidades
 
 - Reconhecimento por voz
 - Seleção de temas
 - Interface propria com botões interativos
-
-## Deploy
-
-A aplicação está disponível no Vercel: [Léo-AI POC Beta](https://chat-gpt-poc-beta.vercel.app/)
 
 ## Como executar
 
--- src/components/NavBar/ToggleLang.tsx
+++ src/components/NavBar/ToggleLang.tsx
@@ -1,20 +1,25 @@
+import { useState } from "react";
 import BRflag from "../Icons/BRflag";
 import USAflag from "../Icons/USAflag";
 
 export default function ToggleLang({ GPT }) {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const handleChangeLang = (selectedLang) => {
         GPT.handleChangeLang(selectedLang);
+        setDropdownOpen(false);
     }
 
     return (
-        <div className="tooltip tooltip-primary tooltip-bottom" data-tip={GPT.lang.langTooltip}>
-            <div className="flex mr-1 justify-center align-middle content-center items-center self-center">
-                <div className="dropdown dropdown-bottom flex justify-center align-middle content-center items-center self-center">
+        <div id='lang-dropdown-3' className="tooltip tooltip-primary tooltip-bottom" data-tip={GPT.lang.langTooltip}>
+            <div id='lang-dropdown-2' className="flex mr-1 justify-center align-middle content-center items-center self-center">
+                <div id='lang-dropdown-1' className="dropdown dropdown-bottom flex justify-center align-middle content-center items-center self-center">
                     <button
+                        id="lang-dropdown"
                         tabIndex={0}
                         role="button"
                         className="btn btn-circle animate-pulse border border-black hover:border-gray-300 hover:animate-none"
+                        onClick={() => setDropdownOpen(!dropdownOpen)}
                     >
                         {GPT.lang.recognitionInstance === 'pt-BR' ?
                             (
@@ -23,31 +28,30 @@ export default function ToggleLang({ GPT }) {
                                 <USAflag />
                             )}
                     </button>
-                    <ul
-                        tabIndex={0}
-                        className="dropdown-content z-[1] p-2 m-0 mr-2 shadow-lg hover:shadow-xl bg-base-300 rounded-box w-26 border-2 border-accent"
-                    >
-                        <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="btn btn-sm btn-block btn-ghost justify-center p-1"
-                                aria-label="PT"
-                                value={GPT.PT}
-                                onChange={() => handleChangeLang(GPT.PT)}
-                            />
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="btn btn-sm btn-block btn-ghost justify-center p-1"
-                                aria-label="EN"
-                                value={GPT.EN}
-                                onChange={() => handleChangeLang(GPT.EN)}
-                            />
-                        </li>
-                    </ul>
+                    {dropdownOpen && (
+                        <ul
+                            id="lang-dropdown0"
+                            tabIndex={0}
+                            className="dropdown-content z-[1] p-2 m-0 mr-2 shadow-lg hover:shadow-xl bg-base-300 rounded-box w-26 border-2 border-accent"
+                        >
+                            <li>
+                                <button
+                                    name="lang-dropdown1"
+                                    className="btn btn-sm btn-primary btn-ghost justify-center p-1"
+                                    aria-label="PT"
+                                    onClick={() => handleChangeLang(GPT.PT)}
+                                >PT</button>
+                            </li>
+                            <li>
+                                <button
+                                    name="lang-dropdown2"
+                                    className="btn btn-sm btn-primary btn-ghost justify-center p-1"
+                                    aria-label="EN"
+                                    onClick={() => handleChangeLang(GPT.EN)}
+                                >EN</button>
+                            </li>
+                        </ul>
+                    )}
                 </div>
             </div>
         </div>
